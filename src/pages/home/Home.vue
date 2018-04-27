56e37d6d4699f0308170014492ce954277fc0570
lamton <707830710@qq.com>
2018-04-27 17:11:31 +0800

    add swiper

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
-   <home-header>
-     this is header
-   </home-header>
+   <home-header></home-header>
+   <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/header'
+import HomeSwiper from './components/swiper'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1708/7c/c0b9acba07764302.jpg_640x200_6cc9f586.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/8b/f7f0585bbb539202.jpg_640x200_c4096429.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+      background #fff !important
+  .wrapper
+    width 100%
+    height 0
+    overflow hidden
+    padding-bottom 31.25%
+    background-color #eee
+    .swiper-img
+      width 100%
+</style>
