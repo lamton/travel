91ca66fb60c9d46cd3bc40a74a632ea4ccbe082c
lamton <707830710@qq.com>
2018-04-28 22:14:17 +0800

    add search

---
+++ src/pages/city/components/search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input type="text" placeholder="输入城市名或拼音" class="search-input">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles'
+  .search
+    height .72rem
+    padding 0 .1rem
+    background $bgColor
+    .search-input
+      box-sizing border-box
+      height .62rem
+      line-height .62rem
+      width 100%
+      text-align center
+      border-radius .1rem
+      padding 0 .2rem
+      color #666
+</style>
