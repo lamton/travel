a915987ce35339cc7c21974009e20164e6eb1fbb
lamton <707830710@qq.com>
2018-05-01 15:59:49 +0800

    finished banner

---
+++ src/pages/detail/components/banner.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <div class="banner" @click="handleBannerClick">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1709/41/411f234d79457081a3.img.jpg_600x330_b5e86902.jpg">
+      <div class="banner-info">
+        <div class="banner-title">长隆野生动物世界(AAAAA景区)</div>
+        <div class="banner-number"><span class="iconfont banner-icon">&#xe6a5;</span>39</div>
+      </div>
+    </div>
+    <common-gallary :imgs="imgs" v-show="showGallary" @close="handleGallaryClose"></common-gallary>
+  </div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/gallary'
+export default {
+  name: 'DetailBanner',
+  data () {
+    return {
+      showGallary: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1605/bc/bc7f6fb8d955d36390.img.jpg_r_800x800_61baef38.jpg', 'http://img1.qunarzz.com/sight/p0/1605/d8/d84681ede0adfa9c90.img.jpg_r_800x800_c61972e9.jpg']
+    }
+  },
+  methods: {
+    handleBannerClick () {
+      this.showGallary = true
+    },
+    handleGallaryClose () {
+      this.showGallary = false
+    }
+  },
+  components: {
+    CommonGallary
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      color #fff
+      background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8))
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+      .banner-number
+        padding 0 .4rem
+        height .32rem
+        line-height .32rem
+        margin-top .14rem
+        border-radius .2rem
+        font-size .24rem
+        background rgba(0,0,0,.8)
+        .banner-icon
+          font-size .24rem
+</style>
